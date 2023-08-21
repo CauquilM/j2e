6963ce77f2ae1c8288b44b2ddd8d41a3fbae2642
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2023-08-21 23:46:31 +0200

    td 05 fin

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -4,6 +4,7 @@
  */
 package servlets;
 
+import Beans.Article;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -28,19 +29,19 @@ public class HomeServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String titre = request.getParameter("titre");
+        String contenu = request.getParameter("contenu");
+        String date = request.getParameter("date");
+        String auteur = request.getParameter("auteur");
+        Article article = new Article();
+        article.setTitre(titre);
+        article.setDateCreation(date);
+        article.setContenu(contenu);
+        article.setAuteur(auteur);
+        request.setAttribute("message", article);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
+.forward(request, response);
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
